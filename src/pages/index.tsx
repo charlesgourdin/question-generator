5892cf5a72a2db0fe6afc8c70a34b599afd80a3e
Charles Gourdin <gourdin.charles@gmail.com>
2023-03-27 21:28:24 +0200

    chore: add analytics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,6 +34,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      
       <main className={styles.main}>
         <header>
           <h1 className='title'>
